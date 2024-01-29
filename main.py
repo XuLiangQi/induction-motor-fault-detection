90f6558bbeae427436acb52087edb14386d08848
XuLiang Qi <76532517+XuLiangQi@users.noreply.github.com>
2024-01-28 20:11:09 -0500

    adding comments

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from app.build_model import build_model
 
 if __name__ == "__main__":
     
+    # Preprocess the data
     X_train, y_train, X_val, y_val, X_test, y_test = pre_process_data()
 
+    # Train the model
     build_model(X_train, y_train, X_val, y_val, X_test, y_test)
